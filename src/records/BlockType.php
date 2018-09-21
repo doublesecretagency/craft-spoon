27cae7891c6d85a867d640e729bec5e274636967
Josh Angell <josh@angell.io>
2018-09-21 11:02:35 +0100

    Fixing spacing issue

--- src/records/BlockType.php
+++ src/records/BlockType.php
@@ -38,18 +38,18 @@ class BlockType extends ActiveRecord
     // Public Static Methods
     // =========================================================================
 
-        /**
-         * Declares the name of the database table associated with this AR class.
-         * By default this method returns the class name as the table name by calling [[Inflector::camel2id()]]
-         * with prefix [[Connection::tablePrefix]]. For example if [[Connection::tablePrefix]] is `tbl_`,
-         * `Customer` becomes `tbl_customer`, and `OrderItem` becomes `tbl_order_item`. You may override this method
-         * if the table is not named after this convention.
-         *
-         * By convention, tables created by plugins should be prefixed with the plugin
-         * name and an underscore.
-         *
-         * @return string the table name
-         */
+    /**
+     * Declares the name of the database table associated with this AR class.
+     * By default this method returns the class name as the table name by calling [[Inflector::camel2id()]]
+     * with prefix [[Connection::tablePrefix]]. For example if [[Connection::tablePrefix]] is `tbl_`,
+     * `Customer` becomes `tbl_customer`, and `OrderItem` becomes `tbl_order_item`. You may override this method
+     * if the table is not named after this convention.
+     *
+     * By convention, tables created by plugins should be prefixed with the plugin
+     * name and an underscore.
+     *
+     * @return string the table name
+     */
     public static function tableName()
     {
         return '{{%spoon_blocktypes}}';
