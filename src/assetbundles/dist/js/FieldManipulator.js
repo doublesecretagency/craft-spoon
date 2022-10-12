3b5b8e3fa1b81e1ca876656d26dcacd916bd81f6
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2022-10-12 15:55:49 -0700

    Compiled JS with recent patch included

--- src/assetbundles/dist/js/FieldManipulator.js
+++ src/assetbundles/dist/js/FieldManipulator.js
@@ -353,9 +353,9 @@ var __webpack_exports__ = {};
     initSettingsMenu: function initSettingsMenu($settingsBtn, spoonedBlockTypes, $matrixField) {
       Garnish.requestAnimationFrame($.proxy(function () {
         // Get the Garnish.MenuBtn object
-        var menuBtn = $settingsBtn.data('menubtn') || false; // If there wasn’t one then fail and try again
+        var disclosureMenuBtn = $settingsBtn.data('trigger') || false; // If there wasn’t one then fail and try again
 
-        if (!menuBtn) {
+        if (!disclosureMenuBtn) {
           this.initSettingsMenu($settingsBtn, spoonedBlockTypes, $matrixField);
           return;
         } // Get the field handle
@@ -364,7 +364,7 @@ var __webpack_exports__ = {};
         var matrixFieldHandle = this._getMatrixFieldName($matrixField); // Get the actual menu out of it once we get this far
 
 
-        var $menu = menuBtn.menu.$container;
+        var $menu = disclosureMenuBtn.$container;
         $menu.addClass('spoon-settings-menu'); // Hide all the li’s with add block links in them
 
         $menu.find('a[data-action="add"]').parents('li').addClass('hidden'); // Remove all the padded classes on hr’s
